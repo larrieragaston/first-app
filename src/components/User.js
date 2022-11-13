f223e06c8a6993f277fd25dbda6fd52985358191
Gaston Larriera <larriera.gaston@gmail.com>
2022-11-13 12:23:35 -0300

    add languageContext

---
+++ src/components/User.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import axios from "axios";
+import { Button } from "antd";
+import userService from "../services/users";
+
+function User({ data, userList, setUserList }) {
+	const { userName, _id } = data;
+	const [userNameModified, setUserNameModified] = useState(data.userName);
+
+	const loguearInfoCompleta = async () => {
+		const { data: userInfo } = await userService.getUserById(_id);
+		console.log("userInfo: ", userInfo);
+	};
+
+	const modificarUsuario = async () => {
+		const { data } = await userService.updateUserById(_id, {
+			userName: userNameModified,
+		});
+		console.log("Response: ", data);
+	};
+
+	const eliminarUsuario = async () => {
+		try {
+			const { data } = await userService.deleteUserById(_id);
+			setUserList(userList.filter((x) => x._id !== _id));
+			console.log("Response: ", data);
+		} catch (err) {
+			console.log("Therre was an error deleting user ", _id);
+			console.log(err);
+		}
+	};
+
+	return (
+		<div>
+			<p>{userName}</p>
+			<input
+				value={userNameModified}
+				onChange={(e) => setUserNameModified(e.target.value)}
+			/>
+			{/* <button onClick={() => loguearInfoCompleta()}>Ver Detalle</button> */}
+			<Button type="primary" onClick={() => loguearInfoCompleta()}>
+				Ver Detalle
+			</Button>
+			<button onClick={() => modificarUsuario()}>Modificar</button>
+			{/* <button onClick={() => eliminarUsuario()}>Borrar</button> */}
+			<Button danger disabled={true} onClick={() => eliminarUsuario()}>
+				Borrar
+			</Button>
+		</div>
+	);
+}
+
+export default User;
---
+++ src/modules/UserList.js
@@ -0,0 +1,117 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import User from "../components/User";
+import { Button, Form, Input, Modal, message } from "antd";
+import { LanguageContext } from "../context/LanguageContext";
+import { useContext } from "react";
+import userService from "../services/users";
+
+const CreateUser = ({ open, onCreate, onCancel }) => {
+	const [form] = Form.useForm();
+
+	return (
+		<Modal
+			open={open}
+			title="Create a new user"
+			okText="Create"
+			cancelText="Cancel"
+			onCancel={onCancel}
+			onOk={() => {
+				form
+					.validateFields()
+					.then((values) => {
+						onCreate(values);
+						form.resetFields();
+					})
+					.catch((info) => {
+						console.log("Validate Failed:", info);
+					});
+			}}
+		>
+			<Form
+				form={form}
+				layout="vertical"
+				name="form_in_modal"
+				initialValues={{
+					modifier: "public",
+				}}
+			>
+				<Form.Item
+					name="email"
+					label="Email"
+					rules={[
+						{ type: "email" },
+						{
+							required: true,
+							message: "Please input the email!",
+						},
+					]}
+				>
+					<Input />
+				</Form.Item>
+			</Form>
+		</Modal>
+	);
+};
+
+function UserList() {
+	const [users, setUsers] = useState([]);
+
+	const { language, setLanguage } = useContext(LanguageContext);
+
+	const [open, setOpen] = useState(false);
+	const error = (errorMessage) => {
+		message.error("Error: ", errorMessage);
+	};
+
+	const onCreate = async (values) => {
+		try {
+			console.log("Received values of form: ", values);
+			const response = await userService.createUser({
+				userName: values.email,
+				role: "admin",
+				isActive: true,
+			});
+			const newUser = response.data;
+			setUsers([...users, newUser]);
+			console.log("Response: ", response);
+			setOpen(false);
+		} catch (err) {
+			error(err);
+		}
+	};
+
+	useEffect(() => {
+		async function fetchData() {
+			const response = await userService.getUsers();
+			setUsers(response.data);
+		}
+		fetchData();
+	}, []);
+
+	return (
+		<div>
+			<p>{language}</p>
+			<Button
+				type="primary"
+				onClick={() => {
+					setOpen(true);
+				}}
+			>
+				Crear Usuario
+			</Button>
+			<CreateUser
+				open={open}
+				onCreate={onCreate}
+				onCancel={() => {
+					setOpen(false);
+				}}
+			/>
+			{users.map((x) => (
+				<User data={x} userList={users} setUserList={setUsers}></User>
+			))}
+		</div>
+	);
+}
+
+export default UserList;
